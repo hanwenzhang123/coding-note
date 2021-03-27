77cfbe74642f05623eb0ceea164eeee53981df14
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-27 14:46:21 -0400

    Update and rename loops-do-while.js to loops-do.while.js

--- loops-do.while.js
+++ loops-do.while.js
@@ -9,5 +9,5 @@ do {
 } while (counter < 10 );
 
 /* do the action over and over again while the condition is true, similar to the while.
-but do while always run before the while.
+but do while always runs a block of code before checking the condition
 use do while when your code at least run one time */
