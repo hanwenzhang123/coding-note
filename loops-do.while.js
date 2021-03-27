9c4872527572fb8d5b9ae4122629c66dffdaecd9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-27 15:08:38 -0400

    Update loops-do.while.js

--- loops-do.while.js
+++ loops-do.while.js
@@ -1,3 +1,9 @@
+do {
+  // code to run
+}
+while ( condition );
+
+
 function getRandomNumber(upper){
 return Math.floor( Math.random() * upper) + 1
 }
@@ -13,6 +19,15 @@ but do while always runs a block of code before checking the condition
 use do while when your code at least run one time */
 
 
+let secret;
+do {
+     secret = prompt("What is the secret password?");
+} while (secret !== 'sesame');
+
+alert("You know the secret password. Welcome!");
+
+
+
 function getRandomNumber(upper) { ... }
 
 let counter = 0;
@@ -20,7 +35,6 @@ do {
   console.log( `The random number is ${getRandomNumber(10)}` );
   counter++; // increment operator
 } while ( counter < 10 );
-
                                  
                                  
 function getRandomNumber(upper) { ... }
