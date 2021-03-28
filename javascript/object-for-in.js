a998bbc0e040c24fdd0aba3dae3356d0626b30e0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-27 21:39:31 -0400

    Create object-for-in.js

---
+++ javascript/object-for-in.js
@@ -0,0 +1,83 @@
+const person = {
+  name: 'Reggie',
+  role: 'Software developer',
+  skills: ['JavaScript', 'HTML', 'CSS'],
+  isTeacher: true
+};
+
+for ( let prop in person ) {
+  console.log(prop);
+}
+
+
+//The Object.keys() method returns an array containing the property names (or keys) of an object
+//Since Object.keys() stores an object's keys inside a new array, you can use it for checking the length of an object
+const person = {
+  name: 'Reggie',
+  role: 'Software developer',
+  skills: ['JavaScript', 'HTML', 'CSS'],
+  isTeacher: true
+};
+
+// Store the keys of the `person` object in `personProps`
+const personProps = Object.keys(person); 
+console.log(personProps);
+> (4) ["name", "role", "skills", "isTeacher"]
+
+
+
+const person = {
+  name: 'Reggie',
+  role: 'Software developer',
+  skills: ['JavaScript', 'HTML', 'CSS'],
+  isTeacher: true
+};
+
+console.log( Object.keys(person).length );  // 4
+
+
+//The Object.values() method returns an array of a given object's property values, in the same order as provided by a for...in loop. 
+const person = {
+  name: 'Reggie',
+  role: 'Software developer',
+  skills: ['JavaScript', 'HTML', 'CSS'],
+  isTeacher: true
+};
+
+// Store the `person` object's property values in `personVals`
+const personVals = Object.values(person); 
+console.log(personVals);
+> (4)
+[
+  "Reggie",
+  "Software developer",
+  ["JavaScript","HTML","CSS"],
+  true
+]
+
+
+const name = {
+  firstName: 'Reggie',
+  lastName: 'Williams',
+};
+
+const role = {
+  title: 'Software developer',
+  skills: ['JavaScript', 'HTML', 'CSS'],
+  isTeacher: true
+};
+
+// merge `name` and `role` into a `person` object
+const person = {  
+  ...name,
+  ...role
+};
+console.log(person);
+
+{
+  firstName: "Reggie",
+  lastName: "Williams",
+  title: "Software developer",
+  skills: ["JavaScript", "HTML", "CSS"],
+  isTeacher: true
+}
