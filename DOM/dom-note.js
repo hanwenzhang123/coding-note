9262182d02eeca7b3e82f661e5da78907e424590
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-28 20:44:30 -0400

    Update and rename dom-note to dom-note.js

---
+++ DOM/dom-note.js
@@ -0,0 +1,23 @@
+Element.textContent
+Element.innerHTML
+Element.attribute
+Element.style.prop
+document.createElement
+Node.appendChild(childElement)
+Nod.removeChild(childElement)
+
+
+//event - we can tie behavior to them
+function exec(func, arg) {
+  func(arg);
+}
+exec((something) => {
+  console.log(something);
+}, 'Greetings, everyone!');
+// one function passes to another function - alloow you more control overe when and how a function gets executed
+
+//timeout
+var timeoutID = scope.setTimeout(function[, delay, arg1, arg2, ...]);
+window.setTimeout((something) => {
+  console.log(something);
+}, 3000, 'Greetings, everyone!');
