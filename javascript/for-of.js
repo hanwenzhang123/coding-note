7bb92fabc029d826ce881fbfedb7a0a8550153e9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-29 18:00:32 -0400

    Create for-of.js

---
+++ javascript/for-of.js
@@ -0,0 +1,16 @@
+const numbers = [1,2,3,4,5,6,7,8,9]; 
+
+for (let sub of numbers)
+{console.log( sub * sub)}
+
+
+
+
+const array1 = ['a', 'b', 'c'];
+
+for (const element of array1) {
+  console.log(element);
+}
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
