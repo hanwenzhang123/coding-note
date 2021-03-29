65479a9da1b4f338888af27841cb1f9dda4dfabd
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-29 18:16:10 -0400

    Update loops-for-of.js

--- javascript/loops-for-of.js
+++ javascript/loops-for-of.js
@@ -35,3 +35,41 @@ for (let i=0; i < paragraphs.length; i++ ) {
 for (let p of paragraphs) {
     p.style.color = 'blue';
 }
+
+
+
+
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
+// }
+
+for (let subreddit of subreddits) {
+    console.log(`Visit reddit.com/r/${subreddit}`)
+}
+
+
+
+// const seatingChart = [
+//     ['Kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//     const row = seatingChart[i];
+//     for (let j = 0; j < row.length; j++) {
+//         console.log(row[j])
+//     }
+// }
+
+// for (let row of seatingChart) {
+//     for (let student of row) {
+//         console.log(student);
+//     }
+// }
+
+// for (let char of "hello world") {
+//     console.log(char)
+// }
