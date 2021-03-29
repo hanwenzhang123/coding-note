6439a1c1e1d65a541914522fd0c30b68a20e8558
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-29 16:32:16 -0400

    Create traversing.js

---
+++ coding-exercise/DOM/traversing.js
@@ -0,0 +1,3 @@
+Select all child elements of section and assign them to the paragraphs variable.
+Next, change the color of each child paragraph to blue.
+(Remember: paragraphs is a collection of elements, so you'll first need to use a loop to access each element in the collection.)
