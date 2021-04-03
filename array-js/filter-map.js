6f1b6b27b72d8ba6e03ed3ea615c7dcf8c89a865
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-03 17:21:28 -0400

    Update filter-map.js

--- array-js/filter-map.js
+++ array-js/filter-map.js
@@ -2,12 +2,31 @@ const userNames = ['Samir', 'Angela', 'Beatrice', 'Shaniqua', 'Marvin', 'Sean'];
   
 const users = userNames
 .filter(name => name.charAt(0) === 'S')
-.map(name => ( {name: name} ));   //{name} is the shorthand for {name: name}
+.map(name => ( {name} ));   //{name} here is the short version for {name: name} 
+//name => ({ 'name': name })
 //arrow function here does not know it is the object literal if we only use { } which is also 
 
 console.log(users)
 
 // Result: [{name: 'Samir'}, {name: 'Shaniqua'}, {name:'Sean'}];
+
+
+
+// cleaner way
+const userNames = ['Samir', 'Angela', 'Beatrice', 'Shaniqua', 'Marvin', 'Sean'];
+
+const users = userNames
+    .filter(name => name[0] === 'S')
+    .map(name => {
+        return "name: " + name;
+    });
+
+console.log(users);
+
+//["name: Samir", "name: Shaniqua", "name: Sean"]
+
+
+
     
     
 const users = [
@@ -20,7 +39,39 @@ const users = [
 ];
 
     // Result: ['Angela', 'Beatrice', 'Shaniqua', 'Sean'];
+
+
+
+
+
+//Using the filter and map methods on the todos array, create an array of unfinished task strings. 
+//See the comments below to see the correct result. Store the new array in the variable unfinishedTasks.
     
-    
-    
-    
+const todos = [
+    {
+        todo: 'Buy apples',
+        done: false
+    },
+    {
+        todo: 'Wash car',
+        done: true
+    },
+    {
+        todo: 'Write web app',
+        done: false
+    },
+    {
+        todo: 'Read MDN page on JavaScript arrays',
+        done: true
+    },
+    {
+        todo: 'Call mom',
+        done: false
+    }
+];
+let unfinishedTasks = todos
+  .filter( if ( arr % 2 !== 0) )
+  .map( arr => { return arr } )
+;
+
+
