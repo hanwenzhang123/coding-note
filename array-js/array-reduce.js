d6ca293dec597575d3bea4d38a15b1c4fd6aae1e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-03 13:49:41 -0400

    Update array-reduce.js

--- array-js/array-reduce.js
+++ array-js/array-reduce.js
@@ -18,7 +18,18 @@ const gNameCount = names.reduce((count, name) => {
   if (name[0] === 'G'){
   return count +1;
   }, 
-    return coount; 
+    return count; 
 }, 0};
     // Result: 4
 
+//phoneNumbers is array of 10 digit phone numbers, where the first three digits, in parentheses, are area codes. 
+//Using reduce, return the total phone numbers with a 503 area code. Store the total in the variable numberOf503.
+
+const phoneNumbers = ["(503) 123-4567", "(646) 123-4567", "(503) 987-6543", "(503) 234-5678", "(212) 123-4567", "(416) 123-4567"];
+let numberOf503 = phoneNumbers.reduce((total, phoneNumberString) => {
+    if (phoneNumberString.slice(1, 4) === "503") {
+    total++;
+  }
+  return total;
+}, 0);
+                                
