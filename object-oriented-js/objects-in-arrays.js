c3be7f94695589949b3a32d10e050fcea4694344
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-03 10:02:10 -0400

    Create objects-in-arrays.js

---
+++ object-oriented-js/objects-in-arrays.js
@@ -0,0 +1,6 @@
+const questions = [
+  { question: 'How many planets are in the Solar System?', answer: '8' },
+  { question: 'How many continents are there?', answer: '7' },
+  { question: 'How many legs does an insect have?', answer: '6' },
+  { question: 'What year was JavaScript created?', answer: '1995' }
+];
