22a35b844084b8238f26b7e8633e07e030bfabae
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-04 14:16:27 -0400

    Update json-parse.js

--- AJAX/json-parse.js
+++ AJAX/json-parse.js
@@ -1,14 +1,13 @@
 
-
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4){
    let employees = JSON.parse(xhr.responseText);  // read-only property, returns the text received from a server following a request being sent
     console.log(employees);    
-    // JSON.parse take string try to convert it to the js object, json.parse won't work if formatting is wrong.
+    // JSON.parse take string try to convert it to the js object, json.parse won't work if formatting is wrong. parses a JSON string.
   }
   xhr.open('GET', 'data/employees.js')
   xhr.send()
   
   
-  
+  //JSON format - key and string values must have double quotes "" and seperate with ,
