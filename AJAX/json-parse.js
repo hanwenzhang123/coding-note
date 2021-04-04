a5a28bb53a2b2e73a672a4ed2698ba2fbd1e47cf
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-04 14:13:50 -0400

    Create json-parse.js

---
+++ AJAX/json-parse.js
@@ -0,0 +1,14 @@
+
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4){
+   let employees = JSON.parse(xhr.responseText);  // read-only property, returns the text received from a server following a request being sent
+    console.log(employees);    
+    // JSON.parse take string try to convert it to the js object, json.parse won't work if formatting is wrong.
+  }
+  xhr.open('GET', 'data/employees.js')
+  xhr.send()
+  
+  
+  
