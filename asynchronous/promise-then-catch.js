7c7f1409a8ce4658e073bc3383922bde9efe364b
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-05 15:49:45 -0400

    Update and rename promise-catch.js to promise-then-catch.js

--- asynchronous/promise-then-catch.js
+++ asynchronous/promise-then-catch.js
@@ -1,3 +1,4 @@
+//new keyword with promise() constructor function
 const breakfastPromise = new Promise ((resolve, reject) => {
   setTimeout(() => {
     resolve('Your order is ready. Come and get it!');
@@ -6,14 +7,14 @@ const breakfastPromise = new Promise ((resolve, reject) => {
 console.log(breakfastPromise);
 
 
-//then - handle both fulfilled or rejected promises
+//then() - handle both fulfilled or rejected promises
 breakfastPromise.then( val => console.log(val) )
+
+//then - the function passed to then() immediate revoke 
 //for 3 seconds, change from pending to fulfill 
 
 
-
-
-//catch - catch the rejected
+//catch() - catch the rejected
 const breakfastPromise = new Promise ((resolve, reject) => {
   setTimeout(() => {
     reject('oh no! there was a problem with your order');
@@ -22,10 +23,11 @@ const breakfastPromise = new Promise ((resolve, reject) => {
 console.log(breakfastPromise);
 breakfastPromise.then( val => console.log(val).catch(err => console.log (err) );
 
+//the error get caught and pass to the function err, the reject message revokes
 
 
                       
-//new version                      
+//new better version with conditional statement                      
 const order = false;
 
 const breakfastPromise = new Promise( (resolve, reject) => {
@@ -33,7 +35,7 @@ const breakfastPromise = new Promise( (resolve, reject) => {
     if (order) {
       resolve('Your order is ready. Come and get it!');
     } else {
-      reject( Error('Your order cannot be made.') );
+      reject( Error('Your order cannot be made.') ); //here we pass an error object to reject versus a string only for debugging purpose, not required. 
     }
   }, 3000);
 });
