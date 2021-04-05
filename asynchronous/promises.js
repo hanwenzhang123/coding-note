308181664704f556bce3ce807c0bb87d88e814bd
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-05 15:36:54 -0400

    Create promises.js

---
+++ asynchronous/promises.js
@@ -0,0 +1,14 @@
+//what is a promise? A promise represents the eventual completion of a asynchronous operation.
+//A value that may not be available yet, like data requested from a server.
+//Different between callbacks and promises
+//A promise represents a value that can be handles at some point later
+//Callback may leave us with some uncertainties about that value
+//A promise will promise or guarantee a future value and nothing can change it regardless of whether or not it comes back fulfilled or rejected
+
+
+
+
+
+
+
+
