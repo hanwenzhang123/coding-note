43abcc63dbde5028fb7c9d0e0dbcb60ce7ccf9d9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-05 20:10:57 -0400

    Create try-catch.js

---
+++ asynchronous/try-catch.js
@@ -0,0 +1,41 @@
+//handle all fetch requests, always use in async and await to handle error
+
+async function getJSON(url){
+  try{
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    throw error;      //any error catch here will bubble up
+  }
+}
+
+
+
+btn.addEventListener('click', event => {
+  event.target.textContent = 'Loading...';
+
+  getPeopleInSpace(astrosUrl)
+    .then(generateHTML)
+    .catch ( e => {
+      peopleList.innerHTML ='<h3>Something went wrong!</h3/';
+      console.error(e)      //print the error message in the console 
+  })
+    .finally( () =>  event.target.remove()  )
+});
+
+
+
+//Using try...catch and async/await in the event listener:
+
+btn.addEventListener('click', async (event) => {
+  event.target.textContent = 'Loading...';
+  try {
+    const astros = await getPeopleInSpace(astrosUrl);
+    generateHTML(astros);
+  } catch(e) {
+    peopleList.innerHTML = '<h3>Something went wrong!</h3>';
+    console.error(e);    
+  } finally {
+    event.target.remove();
+  }
+});
