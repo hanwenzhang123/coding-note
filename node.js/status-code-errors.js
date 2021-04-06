ef6940b0683a2457773a8f5c2131e257909d31f0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-06 15:14:27 -0400

    Create status-code-errors.js

---
+++ node.js/status-code-errors.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+//STATUS_CODES
+The STATUS_CODES array is not on the https object. STATUS_CODES can be found on the http object. To include the STATUS_CODES use the following code:
+
+const http = require('http');
+
+http.STATUS_CODES;
