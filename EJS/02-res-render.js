8b6a279f3413e92820c423fcd8c903e00b8b9a99
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-12 12:11:37 -0400

    Update 02-res-render.js

--- EJS/02-res-render.js
+++ EJS/02-res-render.js
@@ -58,3 +58,23 @@ app.set('view engine', 'ejs');
 
 // here you render `orders` template
 response.render("orders", {orders: orders_json});
+
+What does res.render do and what does the html file look like?
+
+res.render() function compiles your template (please don't use ejs), inserts locals there, and creates html output out of those two things.
+
+Answering Edit 2 part.
+
+// here you set that all templates are located in `/views` directory
+app.set('views', __dirname + '/views');
+
+// here you set that you're using `ejs` template engine, and the
+// default extension is `ejs`
+app.set('view engine', 'ejs');
+
+// here you render `orders` template
+response.render("orders", {orders: orders_json});
+
+//So, the template path is views/ (first part) + orders (second part) + .ejs (third part) === views/orders.ejs
+
+
