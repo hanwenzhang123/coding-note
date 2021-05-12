a182877cc38c71db9d12e140360c0606f5e326f5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-12 11:42:35 -0400

    Update 01-basics.js

--- REST-API/01-basics.js
+++ REST-API/01-basics.js
@@ -16,4 +16,8 @@ DELETE
 })
               
               
-              
+GET /comments - list all comments
+POST /comments - Create a new comment 
+GET /comments/:id - Get one comment (using ID)
+PATCH /comments/:id - Update one comment
+DELETE /comments/:id - Destroy one comment
