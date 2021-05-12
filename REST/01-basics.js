aa640e7be13bd965c0987dc4824298ea76f965d2
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-12 10:37:32 -0400

    Create 01-basics.js

---
+++ REST/01-basics.js
@@ -0,0 +1,19 @@
+GET - read
+ - app.get(function(req, res){
+})
+
+POST - create
+ - app.post(function(req, res){
+})
+
+PUT / PATCH - Update
+ - put: update your database by sending entire entry
+ - patch: only the part of the data that is need to be updated
+ 
+DELETE
+ - detele a specific piece. 
+ - app.delete((function(req, res){
+})
+              
+              
+              
