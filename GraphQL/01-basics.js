63037dccb407685df8b5df88e9c20a9fd0492252
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-27 19:33:18 -0400

    Update 01-basics.js

--- GraphQL/01-basics.js
+++ GraphQL/01-basics.js
@@ -30,3 +30,17 @@ Fields - Properties that comprise the shape of a response
 Type - A collection of fields that make up a specific queryable object.
 
  
+//Questions
+- The shape of a GraphQL response is determined by the Fields you include.
+- GraphQL is very similar to JSON in appearance; however, there are some differences. Which of the following JSON components are NOT required in a GraphQL query?
+  Colons, Commas, Values
+- The "id, title, and tagline" in the below snippet are known as: Field
+query {
+	allMovies {
+		id
+		title
+		tagline
+	}
+}
+- Which of the following is a benefit of using GraphQL over REST?
+  Self-documenting APIs / Fetching nested data in a single request
