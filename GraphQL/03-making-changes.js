6fa24b4d6e029118a7dd9692088ad37f4f3fecf1
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-27 20:28:59 -0400

    Update 03-making-changes.js

--- GraphQL/03-making-changes.js
+++ GraphQL/03-making-changes.js
@@ -21,10 +21,58 @@ mutation{
   }
 }     //will successfully created
   
- 
-  
+
 //Questions
 - Which symbol is used to denote that an argument is required?
   Exclamation point
 - GraphQL mutations can return a result, just like Queries.
 - A GraphQL mutation begins with the Declaration
+
+
+//Mutating with Input Object Types4:32
+Return the Movie object with the nested Director object, and show the original fields.
+
+  type Mutation {
+    createMovie (
+      title: String!
+      tagline: String
+      revenue: Int
+    ): Movie
+    addDirectorToMovie (
+      movieId: ID!
+      director: DirectorInput
+    ): Movie
+  }
+  
+//JSON
+{
+  "directorToAdd": {
+    "name": "Charles"
+  }
+}
+    
+//Query
+mutation($directorToAdd: DirectorInput!){
+  addDirectorToMovie(
+    movieId: "movie_0"
+    director: $directorToAdd    //using the query variable with $
+    ){
+    title
+    tagline
+    directors{
+      id
+      name
+    }
+  }
+}
+
+//Questions
+- Which symbol is used to identify variables inside of a query?
+  Dollar Sign
+- Which of the following describes the difference between Input Object types and Scalar types?
+  Input Object types include their own nested properties
+- Input Object types can include more nested Input Object types.
+- Which language is used to specify query variables? 
+  JSON
+- Which pane is used in Launchpad to provide arguments for our mutation?
+  Query Variables
