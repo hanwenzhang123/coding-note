5b2d05091f4f4437381f56491e0577c72b375e1e
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-03 10:00:33 -0500

    Update note.js

--- DOM/note.js
+++ DOM/note.js
@@ -19,3 +19,15 @@ ParentNode.children
 
 parentNode.firstElementChild
 parentNode.lastElementChild
+
+
+//insertBefore
+const card = document.querySelector('.card');
+const footer = document.querySelector('footer');
+const div = document.createElement('div');
+...
+card.insertBefore(div, footer);
+
+//let insertedNode = parentNode.insertBefore(newNode, referenceNode)
+//insert a div before a footer element as a child of .card
+ 
