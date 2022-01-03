ba7c419404e06496aaa9333db9f4a503602332af
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-03 10:01:22 -0500

    Delete target-tagName.js

--- DOM/target-tagName.js
+++
@@ -1,9 +0,0 @@
-Event.target	
-//reference the element that is first receive the event
-
-document.addEventListener('click',(event) => {
-  condole.log (event.target);  //check the target of the event you click on
-});
-
-event.target.tagName == "TAG NAME ALL CAPITAL"
-//read-only property of the Element interface returns the tag name of the element on which it's called
