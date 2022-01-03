453a249d373df0342cbca36ac9c677d1642d7196
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-03 10:01:12 -0500

    Delete insertBefore.js

--- DOM/insertBefore.js
+++
@@ -1,8 +0,0 @@
-const card = document.querySelector('.card');
-const footer = document.querySelector('footer');
-const div = document.createElement('div');
-...
-card.insertBefore(div, footer);
-
-//let insertedNode = parentNode.insertBefore(newNode, referenceNode)
-//insert a div before a footer element as a child of .card
