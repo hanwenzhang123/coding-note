484dfc16c028762d522dcfd1260ee2a641f61389
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-03 10:01:04 -0500

    Update event-object.js

--- DOM/event-object.js
+++ DOM/event-object.js
@@ -1,3 +1,13 @@
+//Event.target	
+//reference the element that is first receive the event
+
+document.addEventListener('click',(event) => {
+  condole.log (event.target);  //check the target of the event you click on
+});
+
+event.target.tagName == "TAG NAME ALL CAPITAL"
+//read-only property of the Element interface returns the tag name of the element on which it's called
+
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
